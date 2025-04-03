c09d9f8b1fb005e0c4c914b7dd37a750f35305dc
cb-here <cbv934@gmail.com>
2025-04-03 10:01:32 +0530

    add more nodes now

--- src/components/FlowChart.jsx
+++ src/components/FlowChart.jsx
@@ -26,11 +26,40 @@ function FlowChart() {
             style: {
                 color: 'black'
             }
+        }, // adding three more node now
+        {
+            id: '4',
+            position: {x: 100, y: 400},
+            data: {label: 'Forth Node'},
+            style: {
+                color: 'black'
+            }
+        },
+        {
+            id: '5',
+            position: {x: 600, y: 500},
+            data: {label: 'Fifth Node'},
+            style: {
+                color: 'black'
+            }
+        },
+        {
+            id: '6',
+            position: {x: 100, y: 600},
+            data: {label: 'Sixth Node'},
+            style: {
+                color: 'black'
+            }
         },
     ]
     const edges = [
         {id: 'e1-2', source: '1', target: '2', type: 'step', label: 'to the'},
-        {id: 'e2-3', source: '2', target: '3', type: 'step', label: 'to the'}
+        {id: 'e2-3', source: '2', target: '3', type: 'step', label: 'to the'},
+        // lets add three and four also
+        {id: 'e3-4', source: '3', target: '4', type: 'step', label: "to the"},
+        // adding the edges now but in opposite direction 
+        {id: 'e6-5', source: '6', target: '5', type: 'step', label: 'from 6 to 5'},
+        {id: 'e5-4', source: '5', target: '4', type: 'step', label: 'from 5 to 4'},
     ]
   return (
     <div style={{ height: '100%', width: '100%' }}>
