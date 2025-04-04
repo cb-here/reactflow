ff31a5bbad00a6c7429e5b8aaec622e1845cbb72
cb-here <cbv934@gmail.com>
2025-04-04 20:16:45 +0530

    now you could move the node

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import '@xyflow/react/dist/style.css';
 import React from 'react'
 import './App.css'
 import FlowChart from './components/FlowChart';
+import Draggable from './components/Draggable';
 
 function App() {
   return (
       <div style={{ height: '100vh', width: '100vw'}}>
-        <FlowChart />
+        {/* <FlowChart /> */}
+        <Draggable />
       </div>
   )
 }
--- src/components/Draggable.jsx
+++ src/components/Draggable.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { Controls, ReactFlow, applyEdgeChanges, applyNodeChanges } from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
 
 function Draggable() {
+  const [nodes, setNodes] = useState([
+    {id: '1', position: {x: 100, y: 200}, data: {label: 'First Node'}, style: {color: 'black'}},
+    {id: '2', position: {x: 500, y: 300}, data: {label: 'Second Node'}, style: {color: 'black'}},
+  ])
+  const onNodeChages = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  )
+
+  const [edges, setEdges] = useState([
+    {id: 'e1-2', source: '1', target: '2', label: 'to the'}
+  ])
+
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((nds) => applyEdgeChanges(changes, nds)),
+    []
+  )
   return (
-    <div>Draggable</div>
+    <div style={{height: '100%'}}>
+      <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodeChages} onEdgesChange={onEdgesChange}>
+        <Controls style={{color: 'black'}}/>
+      </ReactFlow>
+    </div>
   )
 }
 
